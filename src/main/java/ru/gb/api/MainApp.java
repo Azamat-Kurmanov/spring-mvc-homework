cd4db9bdb1d27d84517fafd966cc887605e8cc72
azama <a.kurmanov@csi.kz>
2022-12-05 04:13:40 +0600

    lesson-6

--- src/main/java/ru/gb/api/MainApp.java
+++ src/main/java/ru/gb/api/MainApp.java
@@ -1,2 +1,21 @@
-package ru.gb.api;public class MainApp {
+package ru.gb.api;
+
+import ru.gb.model.Product;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        SessionFactoryUtils sessionFactoryUtils = new SessionFactoryUtils();
+        sessionFactoryUtils.init();
+        try {
+            ProductDao productDao = new ProductDaoImpl(sessionFactoryUtils);
+//            productDao.saveOrUpdate(new Product(1L, "Картошка", 450));
+            Product product = productDao.findById(1L);
+            System.out.println("product: " + product);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            sessionFactoryUtils.shutdown();
+        }
+    }
 }
